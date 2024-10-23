17c62b70c154add8cc661623bb50be8c9cf01b71
Ergwynt <aday18005@gmail.com>
2024-10-23 16:13:37 +0200

    flin

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
     // Conexion a la base de datos de phpmyadmin
    $con = mysqli_connect(
-        'localhost', // Aqui se inserta el host
-        'root', // Nombre del usuario
-        '', // contraseña
-        'prueba' // Nombre de la base de datos
+        'sql203.infinityfree.com', // Aqui se inserta el host
+        'if0_37567037', // Nombre del usuario
+        'ciclon1324', // contraseña
+        'if0_37567037_prueba' // Nombre de la base de datos
     );
     $con->set_charset('utf8');
 ?>
