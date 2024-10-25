d605cc895c6eacc7c30d2805f2f19d37b271d4fc
Ergwynt <aday18005@gmail.com>
2024-10-25 17:04:25 +0200

    ercabesa

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         include('../php/conexion.php')
-        $email = $_POST['email'];
-        $password =password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $email =isset($_POST['email']);
+        $password =isset($_POST['password'])
 
+        $hash = password_hash($password,PASSWORD_BCRYPT)
         $consulta = "SELECT email,pasword FROM user where email = $email";
-        
+
 
 
     }
-
 ?>
--- php/conexion.php
+++ php/conexion.php
@@ -1,10 +1,16 @@
 <?php
     // Conexion a la base de datos de phpmyadmin
-   $con = mysqli_connect(
-        'sql203.infinityfree.com', // Aqui se inserta el host
-        'if0_37567037', // Nombre del usuario
-        'ciclon1324', // contraseña
-        'if0_37567037_prueba' // Nombre de la base de datos
-    );
+//    $con = mysqli_connect(
+//         'sql203.infinityfree.com', // Aqui se inserta el host
+//         'if0_37567037', // Nombre del usuario
+//         'ciclon1324', // contraseña
+//         'if0_37567037_prueba' // Nombre de la base de datos
+//     );
+$con = mysqli_connect(
+    'localhost', // Aqui se inserta el host
+    'root', // Nombre del usuario
+    '', // contraseña
+    'prueba' // Nombre de la base de datos
+);
     $con->set_charset('utf8');
 ?>
